0e3b9068159806532c899a919df2d90d072c83fe
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-09-12 20:48:09 -0400

    Add repository.ts

---
+++ frontend/src/schema/repository.ts
@@ -0,0 +1,9 @@
+// Define the TypeScript interface for Repository data
+
+export interface Repository {
+  name: string;
+  owner: string;
+  url: string;
+  stars_gained: number;
+  total_stars: number;
+}
